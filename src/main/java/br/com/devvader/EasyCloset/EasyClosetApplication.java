655921583fd8ac340570cc4e9a71c1c694bbf662
JuniorMartins <juniorsoaresmartins@gmail.com>
2022-04-15 19:18:16 -0400

    Ambiente

---
+++ src/main/java/br/com/devvader/EasyCloset/EasyClosetApplication.java
@@ -0,0 +1,13 @@
+package br.com.devvader.EasyCloset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyClosetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyClosetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devvader/EasyCloset/EasyClosetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devvader.EasyCloset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyClosetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
