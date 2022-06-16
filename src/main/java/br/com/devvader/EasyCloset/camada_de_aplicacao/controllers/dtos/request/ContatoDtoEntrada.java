d070c510b56a699f8d5505d567b85c5ff6fbb76e
Junior Martins <juniorsoaresmartins@gmail.com>
2022-06-16 17:19:27 -0400

    Início da implantação de mensagens padronizadas para anotações de bean validation.

--- src/main/java/br/com/devvader/EasyCloset/camada_de_aplicacao/controllers/dtos/request/ContatoDtoEntrada.java
+++ src/main/java/br/com/devvader/EasyCloset/camada_de_aplicacao/controllers/dtos/request/ContatoDtoEntrada.java
@@ -2,20 +2,23 @@ package br.com.devvader.EasyCloset.camada_de_aplicacao.controllers.dtos.request;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-@NoArgsConstructor
 @Data
+@NoArgsConstructor
 public final class ContatoDtoEntrada {
 
     // ---------- ATRIBUTOS DE INSTÂNCIA ---------- //
-    @NotNull
-    @NotEmpty
+    @NotNull(message = "{campo.celular.obrigatorio}")
+    @NotEmpty(message = "{campo.celular.obrigatorio}")
+    @Length(min = 8, max = 15, message = "{campo.celular.tamanho-limite}")
     private String celular;
-    @NotNull
-    @NotEmpty
-    @Email
+    @NotNull(message = "{}")
+    @NotEmpty(message = "{}")
+    @Email(message = "{}")
     private String email;
 }
--- src/main/java/br/com/devvader/EasyCloset/camada_de_aplicacao/controllers/dtos/request/PessoaDtoEntrada.java
+++ src/main/java/br/com/devvader/EasyCloset/camada_de_aplicacao/controllers/dtos/request/PessoaDtoEntrada.java
@@ -2,30 +2,33 @@ package br.com.devvader.EasyCloset.camada_de_aplicacao.controllers.dtos.request;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CPF;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-@NoArgsConstructor
 @Data
+@NoArgsConstructor
 public final class PessoaDtoEntrada {
 
     // ---------- ATRIBUTOS DE INSTÂNCIA ---------- //
-    @NotNull
-    @NotEmpty
+    @NotNull(message = "{campo.nome.obrigatorio}")
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
+    @Length(min = 2, max = 50, message = "{campo.nome.tamanho-limite}")
     private String nome;
-    @NotNull
-    @NotEmpty
+    @NotNull(message = "{campo.sobrenome.obrigatorio}")
+    @NotEmpty(message = "{campo.sobrenome.obrigatorio}")
+    @Length(min = 2, max = 100, message = "{campo.sobrenome.tamanho-limite}")
     private String sobrenome;
-    @NotNull
-    @NotEmpty
-    @CPF
+    @NotNull(message = "{campo.cpf.obrigatorio}")
+    @NotEmpty(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.cpf.valido}")
     private String cpf;
-    @NotNull
+    @NotNull(message = "{campo.contato.obrigatorio}")
     @Valid
     private ContatoDtoEntrada contato;
-    @NotNull
+    @NotNull(message = "{campo.endereco.obrigatorio}")
     @Valid
     private EnderecoDtoEntrada endereco;
 }
