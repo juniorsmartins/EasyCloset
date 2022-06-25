356f3b0af0b2cfbca0139dbf3b8f495c68df14f3
Junior Martins <juniorsoaresmartins@gmail.com>
2022-06-25 10:30:34 -0400

    Criada interface para o Controllador de Pessoa.

---
+++ src/main/java/br/com/devvader/EasyCloset/camada_de_aplicacao/controllers/IPessoaController.java
@@ -0,0 +1,35 @@
+package br.com.devvader.EasyCloset.camada_de_aplicacao.controllers;
+
+import br.com.devvader.EasyCloset.camada_de_aplicacao.controllers.dtos.request.PessoaDtoEntrada;
+import br.com.devvader.EasyCloset.camada_de_aplicacao.controllers.dtos.request.PessoaDtoEntradaAtualizar;
+import br.com.devvader.EasyCloset.camada_de_aplicacao.controllers.dtos.request.PessoaDtoEntradaListar;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+/**
+ * @author JuniorMartins
+ * @version 1
+ */
+
+@RestController
+@RequestMapping("/v1/pessoas")
+public interface IPessoaController {
+
+    @PostMapping
+    @Transactional
+    ResponseEntity<?> cadastrar(@RequestBody @Valid PessoaDtoEntrada pessoaDtoEntrada, UriComponentsBuilder uriBuilder);
+
+    @GetMapping
+    ResponseEntity<?> listar(PessoaDtoEntradaListar pessoaDtoEntradaListar);
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    ResponseEntity<?> deletar(@PathVariable(name = "id") Long id);
+
+    @PutMapping
+    @Transactional
+    ResponseEntity<?> atualizar(@RequestBody @Valid PessoaDtoEntradaAtualizar pessoaDtoEntradaAtualizar);
+}
