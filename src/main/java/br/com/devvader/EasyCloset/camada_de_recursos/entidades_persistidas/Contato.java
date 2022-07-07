5598fa546a9f5befb0b2a921d730eed41c6e9759
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-07-06 21:15:47 -0400

    Contato - ajustes de espaço.

--- src/main/java/br/com/devvader/EasyCloset/camada_de_recursos/entidades_persistidas/Contato.java
+++ src/main/java/br/com/devvader/EasyCloset/camada_de_recursos/entidades_persistidas/Contato.java
@@ -1,6 +1,5 @@
 package br.com.devvader.EasyCloset.camada_de_recursos.entidades_persistidas;
 
-import br.com.devvader.EasyCloset.camada_de_aplicacao.controllers.dtos.request.ContatoDtoEntrada;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
@@ -17,8 +16,10 @@ public final class Contato extends AbstractAuditingEntity implements Serializabl
     @Id
     @Column(name = "pessoa_id")
     private Long pessoaId;
+
     @Column(name = "celular", length = 25, nullable = false)
     private String celular;
+
     @Column(name = "email", length = 100, nullable = false)
     private String email;
 
@@ -26,10 +27,4 @@ public final class Contato extends AbstractAuditingEntity implements Serializabl
     @MapsId
     @JoinColumn(name = "pessoa_id")
     private Pessoa pessoa;
-
-    // ---------- CONSTRUTORES ---------- //
-    public Contato(ContatoDtoEntrada contatoDtoEntrada) {
-        setCelular(contatoDtoEntrada.getCelular());
-        setEmail(contatoDtoEntrada.getEmail());
-    }
 }
