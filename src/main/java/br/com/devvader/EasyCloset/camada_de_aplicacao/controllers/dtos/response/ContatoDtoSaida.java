31a24e67b97ddc6c0d1802d09803cf1926fe1afe
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-07-07 16:54:00 -0400

    ContatoDtoSaida - retirados campos de auditoria.

--- src/main/java/br/com/devvader/EasyCloset/camada_de_aplicacao/controllers/dtos/response/ContatoDtoSaida.java
+++ src/main/java/br/com/devvader/EasyCloset/camada_de_aplicacao/controllers/dtos/response/ContatoDtoSaida.java
@@ -1,17 +1,14 @@
 package br.com.devvader.EasyCloset.camada_de_aplicacao.controllers.dtos.response;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
+@Getter
 @Setter
 @NoArgsConstructor
 public final class ContatoDtoSaida
 {
     private String celular;
     private String email;
-    private LocalDateTime dataDeCriacao;
-    private LocalDateTime dataDaUltimaModificacao;
 }
