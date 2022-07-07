a001585de480d918e4a68313c603ae9e344e2895
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-07-06 21:17:00 -0400

    Pessoa - adição de anotação Builder.

--- src/main/java/br/com/devvader/EasyCloset/camada_de_recursos/entidades_persistidas/Pessoa.java
+++ src/main/java/br/com/devvader/EasyCloset/camada_de_recursos/entidades_persistidas/Pessoa.java
@@ -1,14 +1,16 @@
 package br.com.devvader.EasyCloset.camada_de_recursos.entidades_persistidas;
 
-import br.com.devvader.EasyCloset.camada_de_aplicacao.controllers.dtos.request.PessoaDtoEntrada;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "pessoas")
-@Data
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
 @NoArgsConstructor
 public final class Pessoa extends AbstractAuditingEntity implements Serializable {
 
@@ -18,25 +20,21 @@ public final class Pessoa extends AbstractAuditingEntity implements Serializable
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "pessoa_id", nullable = false, updatable = false)
     private Long pessoaId;
+
     @Column(name = "nome", length = 25, nullable = false)
     private String nome;
+
     @Column(name = "sobrenome", length = 40, nullable = false)
     private String sobrenome;
+
     @Column(name = "cpf", length = 14, unique = true, nullable = false)
     private String cpf;
 
     @OneToOne(mappedBy = "pessoa", cascade = {CascadeType.ALL}, orphanRemoval = true, fetch = FetchType.LAZY)
     @PrimaryKeyJoinColumn
     private Contato contato;
+
     @OneToOne(mappedBy = "pessoa", cascade = {CascadeType.ALL}, orphanRemoval = true, fetch = FetchType.LAZY)
     @PrimaryKeyJoinColumn
     private Endereco endereco;
-
-    public Pessoa(PessoaDtoEntrada pessoaDtoEntrada) {
-        setNome(pessoaDtoEntrada.getNome());
-        setSobrenome(pessoaDtoEntrada.getSobrenome());
-        setCpf(pessoaDtoEntrada.getCpf());
-        setContato(new Contato(pessoaDtoEntrada.getContato()));
-        setEndereco(new Endereco(pessoaDtoEntrada.getEndereco()));
-    }
 }
