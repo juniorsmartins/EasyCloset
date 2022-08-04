22154ee6b4c0afe72edf44eea798520c7d7571a8
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-08-04 11:32:36 -0400

    Create - criado enum para definir forma de pagamento.

---
+++ src/main/java/br/com/devvader/EasyCloset/camada_de_recursos/entidades_persistidas/roupa/FormaPgtoEnum.java
@@ -0,0 +1,19 @@
+package br.com.devvader.EasyCloset.camada_de_recursos.entidades_persistidas.roupa;
+
+public enum FormaPgtoEnum {
+
+    CARTAO("Cartão"),
+    PIX("Pix"),
+    BOLETO("Boleto"),
+    DINHEIRO("Dinheiro");
+
+    private String valor;
+
+    FormaPgtoEnum(String valor) {
+        this.valor = valor;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+}
